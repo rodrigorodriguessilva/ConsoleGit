7d5be1afdba9858376f267ce92468d1a47dcefc7
rodrigo.rsilv11 <rodrigo.rsilv11@ITA0635414W10-1>
2023-03-28 21:23:26 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
