1dd504c9fbd85b15aad85649d4e5834233cdcae2
RODRIGO RODRIGUES DA SILVA <rodrigo.rsilv11@senacsp.edu.br>
2023-03-28 21:50:12 -0300

    Incluido classe sala3

--- ConsoleGit/Program.cs
+++ ConsoleGit/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("oLA SALA 3!");
         }
     }
 }
---
+++ ConsoleGit/sALA3.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleGit
+{
+    class sALA3
+    {
+    }
+}
